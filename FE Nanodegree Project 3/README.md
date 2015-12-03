b609c24baaac5d4d5ee8abae178aef0a671be905
Steve O'Gallagher <steve.ogallagher@cohaesus.co.uk>
2015-12-03 22:37:16 +0000

    Add instructions to the readme file and add clear comments to the app.js code.

--- FE Nanodegree Project 3/README.md
+++ FE Nanodegree Project 3/README.md
@@ -4,3 +4,25 @@ frontend-nanodegree-arcade-game
 Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
 
 for self-checking their submission.
+
+
+
+INSTRUCTIONS: How to Play This Game
+===================================
+
+Step 1: 
+Start the game by opening the 'index.html' file in your web browser.
+
+Step 2:
+Play the game by pressing the up, down, left, and right keys on your computer's keyboard.
+
+Controls
+--------
+- The Up key will move the player upwards and towards the water.
+- The Down key will move the player downwards and away from the water.
+- The Left key will move the player left and towards the left side of the game scene.
+- The Right key will move the player right and towards the right side of the game scene.
+
+Game Rules
+----------
+In this game you have a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down. The enemies move in varying speeds on the paved block portion of the scene. Once a the player collides with an enemy, the game is reset and the player moves back to the start square and causing you to lose 10 points. Once the player reaches the water, you are awarded 20 points. 
--- FE Nanodegree Project 3/js/app.js
+++ FE Nanodegree Project 3/js/app.js
@@ -1,48 +1,53 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    // Variables applied to each of the Enemy instances go here
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // The image/sprite for our enemies uses a helper provided 
+    // elsewhere in the code to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = -100;
 
     this.newLocation();
     this.newSpeed();
-
 };
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Any movement is multiplied by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // if the Enemy has not yet crossed the screen, move it along
     if (this.x < 500) {
         this.x = this.x + (this.speed * dt);
+    // otherwise, reset its location and give it a new speed.
     } else {
         this.x = -100;
         this.newLocation();
         this.newSpeed();
     };
 
+    // if the Enemy collide with the Player's position, update the score
+    // and prepare to reset the Player's position
     if ((this.y == player.y) && (player.x  > this.x) && (player.x < this.x + 20)) {
         collision = true;
         player.currentScore = player .currentScore - 10;
     };
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Give the evemy a new location
 Enemy.prototype.newLocation = function() {
     var yLocations = [65, 145, 225];
     this.y = yLocations[Math.floor(Math.random() * yLocations.length)];
 };
 
+// Give the eney a new speed
 Enemy.prototype.newSpeed = function() {
     var max_speed = 500;
     var min_speed = 200;
@@ -50,19 +55,19 @@ Enemy.prototype.newSpeed = function() {
 };
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// The Player the user controls is created
 var Player = function() {
 
     this.sprite = 'images/char-boy.png';
     this.reset();
 
+    // Create the initial score of 0 points
     this.currentScore = 0;
     this.score = "Score: " + String(this.currentScore);
-    //this.drawScore();
 };
 
+// Check to see if a collision has been detected and adjust 
+// the game accordingly if so.
 Player.prototype.update = function(dt) {
 
     if (collision == true) {
@@ -70,9 +75,9 @@ Player.prototype.update = function(dt) {
         this.reset();
         collision = false;
     };
-
 }
 
+// Draw the Player and the score on the canvas
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
@@ -80,7 +85,6 @@ Player.prototype.render = function() {
 
     ctx.font = "20pt Impact";
     ctx.textAlign = "center";
-
     ctx.fillStyle = "white";
     ctx.fillText(this.score, 430, 580);
     ctx.strokeStyle = "black";
@@ -88,49 +92,51 @@ Player.prototype.render = function() {
     ctx.strokeText(this.score, 430, 580);
 }
 
+// Move the Player in the game according to the user's input
 Player.prototype.handleInput = function(key) {
+    // Move left
     if (key == "left" && this.x > 0) {
         this.x = this.x - 100;
+    // More right
     } else if (key == "right" && this.x < 400) {
         this.x = this.x + 100;
-
+    // Move up
     } else if (key == "up") {
         if (this.y > 100) {
             this.y = this.y - 80; 
+        // If the Player reached the top of the map, award points
         } else {
             this.currentScore = this.currentScore + 20;
             this.render();
             this.reset();
         }
+    // Move down
     } if (key == "down" && this.y < 375) {
         this.y = this.y + 80;
     }
 };
 
+// Reset the Player's position to the start of the game
 Player.prototype.reset = function() {
     this.x = 200;
     this.y = 385;
 };
 
-// Now instantiate your objects.
+// Instantiate objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var enemy0 = new Enemy();
 var enemy1 = new Enemy();
 var enemy2 = new Enemy();
-
 var allEnemies = [enemy0, enemy1, enemy2];
+
+// Place the player object in a variable called player
 var player = new Player();
 
+// Initialise the collision variable
 var collision = false;
 
-
-
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to the
+// Player.handleInput() method. Y
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
